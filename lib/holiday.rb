327f401ea2ce5c16635d6d90951aea39aaa5652c
Maverick Adolfo <merkonical@gmail.com>
2019-08-28 07:44:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,6 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
       puts" #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
-          binding.pry 
     end
   end 
 end
